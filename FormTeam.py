cc4b2d9ebd97292ca63170ff89dccd1b883ab2d2
JoJaJones <48601585+JoJaJones@users.noreply.github.com>
2021-05-10 15:56:39 -0700

    Create FormTeam.py

---
+++ FormTeam.py
@@ -0,0 +1,15 @@
+def formTeam(d, t, n):
+    dev_arr = [0]*(d-1)
+    test_arr = [0]*(t-1)
+    dev_arr[0] = 1
+    test_arr[0] = 1
+    for i in range(1, n):
+        d_sum = sum(dev_arr) % 1000000007
+        t_sum = sum(test_arr) % 1000000007
+        dev_arr = [t_sum] + dev_arr
+        test_arr = [d_sum] + test_arr
+        dev_arr.pop()
+        test_arr.pop()
+        print(d_sum, t_sum)
+
+    return (sum(dev_arr) + sum(test_arr)) % 1000000007
